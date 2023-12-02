1c88825969020b402756adc1bdbaa1eb7638550c
Ali Shahbazi <ali.vortex799@gmail.com>
2023-12-02 02:50:14 -0500

    Phone validation services

---
+++ AuctriaApplication.Services.Validation/Services/Phone/PhoneValidationService.cs
@@ -0,0 +1,22 @@
+﻿using PhoneNumbers;
+
+namespace AuctriaApplication.Services.Validation.Services.Phone;
+
+public static class PhoneValidationService
+{
+    public static bool IsPhoneNumberValid(string phoneNumber)
+    {
+        var util = PhoneNumberUtil.GetInstance();
+        var region = GetPhoneRegion(phoneNumber);
+        var numberProto = util.Parse(phoneNumber, region);
+
+        return util.IsValidNumber(numberProto);
+    }
+
+    private static string GetPhoneRegion(string phoneNumber)
+    {
+        var util = PhoneNumberUtil.GetInstance();
+        var numberProto = util.Parse(phoneNumber, null);
+        return util.GetRegionCodeForNumber(numberProto);
+    }
+}
