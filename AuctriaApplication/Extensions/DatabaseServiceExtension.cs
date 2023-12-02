84e6deb5aa06cff9948be9a23d4a03e854b68301
Ali Shahbazi <ali.vortex799@gmail.com>
2023-12-02 02:50:52 -0500

    Program extensions

---
+++ AuctriaApplication/Extensions/DatabaseServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using AuctriaApplication.DataAccess.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctria_Application.Extensions;
+
+public static class DatabaseServiceExtension
+{
+    public static IServiceCollection AddDatabaseService(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContextFactory<ApplicationDbContext>(opt => 
+            { opt.UseSqlServer(config.GetConnectionString("DefaultConnection")); });
+        
+        return services;
+    }
+}
---
+++ AuctriaApplication/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System.Security.Claims;
+using System.Text;
+using AuctriaApplication.DataAccess.DbContext;
+using AuctriaApplication.DataAccess.Entities;
+using AuctriaApplication.DataAccess.Entities.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Auctria_Application.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityService(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddIdentity<User, Role>(opt =>
+            {
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 4;
+                opt.User.RequireUniqueEmail = true;
+            })
+            .AddRoles<Role>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "localhost",
+                    ValidAudience = "localhost",
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTSettings:TokenKey"]!)),
+                    TokenDecryptionKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTSettings:EncryptKey"]!))
+                };
+                opt.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(context.Exception, "Authentication failed.");
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = async context =>
+                    {
+                        var signInManager = context.HttpContext.RequestServices
+                            .GetRequiredService<SignInManager<User>>();
+                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+
+                        var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
+                        if (claimsIdentity?.Claims?.Any() != true)
+                        {
+                            logger.LogWarning("Token has no claims.");
+                            context.Fail("This token has no claims.");
+                        }
+
+                        var securityStamp = claimsIdentity?.FindFirst(new ClaimsIdentityOptions().SecurityStampClaimType);
+                        if (securityStamp == null)
+                        {
+                            logger.LogWarning("Token has no security stamp.");
+                            context.Fail("This token has no security stamp");
+                        }
+
+                        var validatedUser = await signInManager.ValidateSecurityStampAsync(context.Principal);
+                        if (validatedUser == null)
+                        {
+                            logger.LogWarning("Token security stamp is not valid.");
+                            context.Fail("Token security stamp is not valid.");
+                        }
+                    },
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+                            context.Token = accessToken;
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
